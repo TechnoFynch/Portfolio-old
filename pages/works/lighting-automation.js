0c8896faafaf540c831444c55b10c6d8d9de0eb1
Shubham Salunke <shubhamdeepak007@gmail.com>
2022-06-25 10:22:46 +0530

    Removed unused imports

--- pages/works/lighting-automation.js
+++ pages/works/lighting-automation.js
@@ -1,14 +1,11 @@
 import {
     Container,
     Badge,
-    Link,
     List,
     ListItem
 } from "@chakra-ui/react";
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 import {
     Title,
-    WorkImage,
     Meta
 } from '../../components/work'
 import P from '../../components/paragraph'
