6c7c28dcaeb8f20884a98905fac7ac589b5092bc
Shubham Salunke <shubhamdeepak007@gmail.com>
2022-08-01 17:40:21 +0530

    Added achievements

--- pages/achievements.js
+++ pages/achievements.js
@@ -13,7 +13,7 @@ const Works = () => {
         <Layout>
             <Container>
                 <Heading as='h3' fontSize={20} mb={4}>
-                    Achievements
+                    Achievements (Images to be uploaded)
                 </Heading>
 
                 <SimpleGrid columns={[1,1,2]} gap={6}>
@@ -25,6 +25,22 @@ const Works = () => {
                     <Section delay={0.3}>
                         <WorkGridItem id='subroto' title='International Tournament' thumbnail={hackathon}>
                             Participated in the International Subroto Cup Football Tournament in 2016
+                        </WorkGridItem>
+                    </Section>
+                    <Section delay={0.3}>
+                        <WorkGridItem id='rfys' title='RFYS Athletics' thumbnail={hackathon}>
+                            Secured second position in the National Reliance Federation Youth Sports 4x400m relay
+                        </WorkGridItem>
+                    </Section>
+                    <Section delay={0.3}>
+                        <WorkGridItem id='toi' title='TOI T20 Soccer' thumbnail={hackathon}>
+                            Participated in multiple Times of India T20 Soccer Tournaments
+                        </WorkGridItem>
+                    </Section>
+                    <Section delay={0.3}>
+                        <WorkGridItem id='local' title='Best Athlete' thumbnail={hackathon}>
+                            Broke 3 school records in 200m, 400m, 4x100m relay events,
+                            Secured Athlete of the Year 2018
                         </WorkGridItem>
                     </Section>
                 </SimpleGrid>
